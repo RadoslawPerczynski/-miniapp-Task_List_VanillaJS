c48364b9468fe48863f746aafe3be32d432f98fc
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2018-10-16 22:47:18 +0200

    added alertify + text constants + filtering and clearing the tasks

--- app.js
+++ app.js
@@ -2,41 +2,47 @@
 const taskInput = document.querySelector('#task');
 const addTaskForm = document.querySelector('#task-form');
 const listOfTasks = document.querySelector('ul.collection');
+const clearTasksBtn = document.querySelector('.clear-tasks');
+const filterInput = document.querySelector('#filter');
+
 
 const taskItemClass = 'collection-item';
 const deleteAnchorClass = 'secondary-content delete-item';
-const emptyTaskMessageText = 'A task cannot be empty. Enter the name.';
+const emptyTaskNotificationText = 'Enter the task\'s name.';
+const taskCreatedNotificationText = 'Task has been successfuly created!';
+const deleteItemConfirmText = 'Are you sure?';
+const clearAllTasksConfirmText = 'Delete the whole list?';
 
-var elem;
-var modal;
+const taskRemovedNotificationText = 'Task has been removed.';
+const clearedAllTasksNotificationText = 'Tasks have been removed.';
+
+const listOfTasksEmptyNotificationText = 'List of tasks is already empty.';
+
+const dialogAnimationEffect = 'fade';
+
+
 
 addEventListeners()
 
 function addEventListeners() {
   addTaskForm.addEventListener('submit', addTask);
   listOfTasks.addEventListener('click', deleteTask);
-
-
-  //modal initialize
-  elem = document.querySelector('.modal');
-  modal = M.Modal.init(elem);
+  clearTasksBtn.addEventListener('click', clearAllTasks);
+  filterInput.addEventListener('input', filterTasks)
 }
 
-function openDialog(text) {
-  const modalText = document.querySelector('.modal-content p');
-  modalText.textContent = text;
-  modal.open();
-}
 
 function addTask(e) {
   e.preventDefault();
   const inputValue = taskInput.value;
 
   if(!inputValue) {
-    openDialog(emptyTaskMessageText);
+    notifyTheUser(emptyTaskNotificationText, 'error');
 
   } else {
-    var newTask = createTaskItem(inputValue);
+    createTaskItem(inputValue);
+    notifyTheUser(taskCreatedNotificationText)
+ 
   }
   
 }
@@ -61,12 +67,58 @@ function deleteTask(e) {
   e.preventDefault();
   const taskItemLi = e.target.parentElement.parentElement;
 
-  if(e.target.parentElement.classList.contains('delete-item')) {
+  if(taskItemLi.classList.contains('collection-item')) {
     
-    if(confirm('Are you sure?')) {
-      taskItemLi.remove();
-    }
-    
+    alertify.confirm(deleteItemConfirmText, function(){ 
+        taskItemLi.remove();
+        notifyTheUser(taskRemovedNotificationText);
+    }).set({transition: dialogAnimationEffect}).setHeader('Confirm');
   }
 
 }
+
+function clearAllTasks() {
+  if((listOfTasks === undefined || listOfTasks.childElementCount == 0)) {
+    notifyTheUser(listOfTasksEmptyNotificationText, 'warning');
+  } else {
+    alertify.confirm(clearAllTasksConfirmText, function(){
+      i=0;
+      while(listOfTasks.firstChild) {
+        listOfTasks.removeChild(listOfTasks.firstChild)
+      };
+      notifyTheUser(clearedAllTasksNotificationText);
+  }).set({transition: dialogAnimationEffect}).setHeader('Confirm');
+  }
+}
+
+function filterTasks(e) {
+  const filterInputValue = e.target.value.toLowerCase();
+
+  const tasksArray = Array.from(listOfTasks.children);
+
+  tasksArray.forEach(function(task) {
+    let taskName = task.firstChild.textContent;
+    if(taskName.toLowerCase().indexOf(filterInputValue) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+
+
+}
+
+//notifications
+function notifyTheUser(text = 'Ok', type = 'success') {
+  alertify.notify(text, type, 5);
+}
+
+// function getAndSetArrayOfTasks() {
+
+//   if(localStorage.getItem('tasks') === 'indefined') {
+//     tasks = [];
+//   } else {
+//     tasks = JSON.parse(localStorage.getItem('tasks'));
+//   }
+
+// }
